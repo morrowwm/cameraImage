197ecbd63c82727649bb44289aee14ecd690421d
morrowwm <morrowwm@yahoo.ca>
2024-01-15 15:21:38 -0400

    fixed lock check location

--- download_motions.py
+++ download_motions.py
@@ -4,11 +4,19 @@ from pytapo.media_stream.downloader import Downloader
 import asyncio
 import os
 import datetime
+from flock import flock
 
-# mandatory
-outputDir = "home/fred/cam/images/motion/"
-host = "192.168.0.8"
-password_cloud = "yabbadabba"
+lock = flock('download_motions.lock', True).acquire()
+if lock:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(download_motions())
+else:
+    print('exiting')
+    exit()
+    
+outputDir = "/opt2/surveillance/eyeball2/motions/" #os.environ.get("OUTPUT")  # directory path where videos will be saved
+host = "192.168.0.175" #os.environ.get("HOST")  # change to camera IP
+password_cloud = "poopoopaper666" #os.environ.get("PASSWORD_CLOUD")  # set to your cloud password
 
 # optional
 window_size = os.environ.get(
@@ -18,7 +26,7 @@ window_size = os.environ.get(
 print("Connecting to camera...")
 tapo = Tapo(host, "admin", password_cloud, password_cloud)
 
-async def download_async():
+async def download_motions():
     print("Getting recordings...")
     now = datetime.datetime.now()
     delta = datetime.timedelta(days=1)
@@ -56,6 +64,5 @@ async def download_async():
                     )
                 print("")
     
-
 loop = asyncio.get_event_loop()
-loop.run_until_complete(download_async())
+loop.run_until_complete(download_motions())
