cd05116cf1a0a49d9e953ceb4415874581c8cab0
morrowwm <morrowwm@yahoo.ca>
2024-01-18 20:35:38 -0400

    Add files via upload

--- download_motions.py
+++ download_motions.py
@@ -7,16 +7,13 @@ import datetime
 from flock import flock
 
 lock = flock('download_motions.lock', True).acquire()
-if lock:
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(download_motions())
-else:
+if not lock:
     print('exiting')
     exit()
     
-outputDir = "/opt2/surveillance/eyeball2/motions/" #os.environ.get("OUTPUT")  # directory path where videos will be saved
-host = "192.168.0.175" #os.environ.get("HOST")  # change to camera IP
-password_cloud = "poopoopaper666" #os.environ.get("PASSWORD_CLOUD")  # set to your cloud password
+outputDir = "XXX/motions/" #os.environ.get("OUTPUT")  # directory path where videos will be saved
+host = "192.168.XXX.YYY" #os.environ.get("HOST")  # change to camera IP
+password_cloud = "XXX" #os.environ.get("PASSWORD_CLOUD")  # set to your cloud password
 
 # optional
 window_size = os.environ.get(
@@ -29,14 +26,21 @@ tapo = Tapo(host, "admin", password_cloud, password_cloud)
 async def download_motions():
     print("Getting recordings...")
     now = datetime.datetime.now()
-    delta = datetime.timedelta(days=1)
-    for days in (0,1):
+    
+    for days in (0,1,2):
         date = now.strftime("%Y%m%d") 
+        delta = datetime.timedelta(days=days)
         now -= delta 
         recordings = tapo.getRecordings(date)
         timeCorrection = tapo.getTimeCorrection()
         for recording in recordings:
             for key in recording:
+                print("start: {0} end: {1} length: {2}".format(
+                    recording[key]["startTime"], recording[key]["endTime"], recording[key]["endTime"] - recording[key]["startTime"]))
+                if int(recording[key]["endTime"]) - int(recording[key]["startTime"]) > 10:
+                    print("Setting endTime to 10s for large video")
+                    recording[key]["endTime"] = recording[key]["startTime"] + 10
+                
                 downloader = Downloader(
                     tapo,
                     recording[key]["startTime"],
